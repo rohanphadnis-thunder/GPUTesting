4c02a2a4754c300eb14f679e9d6fa2516194737d
Rohan Phadnis <rohan@thundercompute.com>
2026-07-23 17:07:02 +0000

    debug

--- run1.sh
+++ run1.sh
@@ -1,4 +1,3 @@
-curl -fsSL https://get.thundercompute.com/install.sh | sudo THUNDER_INSTALL_MODE=client THUNDER_ENROLLMENT_TOKEN='tr_dd7a7fb96cad5cefb5b8f26c0416e265fec52f104d15fbbeb013b68f20cbe808' THUNDER_CENTRAL_URL='https://giga-staging.thundercompute.com:2096' sh
 printf "ld preload: %s\n", $LD_PRELOAD
 printf "using thunder: %s\n", $USING_THUNDER
 ls /etc
