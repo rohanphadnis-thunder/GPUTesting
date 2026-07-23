4382eb454a872e267895a0775dde66f56ed4baff
Rohan Phadnis <rohan@thundercompute.com>
2026-07-23 12:40:38 +0000

    debug

--- run1.sh
+++ run1.sh
@@ -1,5 +1,7 @@
-printf "ld preload: %s\n", LD_PRELOAD
+printf "ld preload: %s\n", $LD_PRELOAD
 printf "using thunder: %s\n", $USING_THUNDER
 ls /etc
 ls /etc/thunder
+find / -type f -name "libthunder.so"
+sudo find / -type f -name "libthunder.so"
 nvidia-smi
