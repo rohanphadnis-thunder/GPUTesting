28e0b523a92a91c1ceb22d9ff90e65d095cc4325
Rohan Phadnis <rohan@thundercompute.com>
2026-07-28 18:59:41 +0000

    added more prints

--- run1.sh
+++ run1.sh
@@ -1,11 +1,13 @@
 printf "hostname: %s\n", $HOSTNAME
 printf "ld preload: %s\n", $LD_PRELOAD
 printf "using thunder: %s\n", $USING_THUNDER
+printf "\n/etc paths:\n"
 ls /etc
+printf "\n/etc/thunder paths:\n"
 ls /etc/thunder
+printf "\n/dev paths:\n"
 ls /dev
-find / -type f -name "libthunder.so"
-sudo find / -type f -name "libthunder.so"
+printf "\nconfig.json content:\n"
+cat /etc/thunder/config.json
+printf "\n"
 nvidia-smi
-
-find / -name "nvidia-smi"
